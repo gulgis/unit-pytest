df8c2c4093334976c372715ab1d585f4407eebd3
willian gulgielmin <willian.gulgielmin@embrapii.org.br>
2021-04-20 18:09:12 -0300

    test and validate data on Point class

--- scripts/chp2/video3/mapmaker_exceptions_start.py
+++ scripts/chp2/video3/mapmaker_exceptions_start.py
@@ -1,9 +1,12 @@
 
 class Point():
     def __init__(self, name, latitude, longitude):
+        if not (-90 <= latitude <= 90) or not (-180 <= longitude <= 180):
+            raise ValueError("Invalid latitude or latitude")
         self.name = name
         self.latitude = latitude
         self.longitude = longitude
+
 
 
     def get_lat_long(self):
--- scripts/chp2/video4/mapmaker_challenge.py
+++ scripts/chp2/video4/mapmaker_challenge.py
@@ -2,10 +2,14 @@
 
 class Point():
     def __init__(self, name, latitude, longitude):
+        if not isinstance(name, str):
+            raise TypeError("Provided name must be a string")
         self.name = name
 
         if not (-90 <= latitude <= 90) or not (-180 <= longitude <= 180):
             raise ValueError("Invalid latitude, longitude combination.")
+        
+        
         self.latitude = latitude
         self.longitude = longitude
 
