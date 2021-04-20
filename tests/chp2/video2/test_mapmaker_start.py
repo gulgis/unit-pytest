f594aa287689ce0a2d23b06f9a3cafcd0e3cf5fe
willian gulgielmin <willian.gulgielmin@embrapii.org.br>
2021-04-20 17:24:00 -0300

    ADD test point class

--- tests/chp2/video2/test_mapmaker_start.py
+++ tests/chp2/video2/test_mapmaker_start.py
@@ -1,4 +1,6 @@
+from scripts.chp2.video2.mapmaker_start import Point
 
 
 def test_make_one_point():
-    pass
+    point_01 = Point('Brasília', -15.7784, -47.9286)
+    assert point_01.get_lat_long() == (-15.7784, -47.9286)
